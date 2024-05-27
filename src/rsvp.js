68d7d18943765fcc733f8b5428692fe0bbdfbb1b
RJ Jaisimha <jaisimha@amazon.com>
2024-05-27 02:06:13 +0200

    Update font sizes for bottom sheet and implement showing RSVP status for input name

--- src/rsvp.js
+++ src/rsvp.js
@@ -6,29 +6,29 @@ function updateText(text) {
   const para = document.getElementById("output");
   para.innerText = text;
 }
-function createGraphqlRequest(successCallback) {
+function createGraphqlRequest() {
   const xhr = new XMLHttpRequest();
   xhr.open("POST", "https://ceremony-api.withjoy.com/graphql");
   xhr.setRequestHeader("Content-Type", "application/json");
-  xhr.onload = () => {
-    if (xhr.readyState == 4 && xhr.status == 200) {
-      successCallback(JSON.parse(xhr.responseText));
-    } else {
-      console.log(`Error: ${xhr.status}`);
-    }
-  };
   return xhr;
 }
-function queryGuestDetails(guestId, successCallback) {
-  const request = createGraphqlRequest((response) => {
-    successCallback(response.data.event.guestQuestions);
+function queryGuestDetails(guestId) {
+  return new Promise((resolve, reject) => {
+    const request = createGraphqlRequest();
+    request.onload = () => {
+      if (request.readyState == 4 && request.status == 200) {
+        resolve(JSON.parse(request.responseText).data.event.guestQuestions);
+      } else {
+        reject(`Error: ${request.status}`);
+      }
+    };
+    const body = JSON.stringify({
+      "query":"query($eventId:ID!){\nevent(eventId:$eventId){\nguestQuestions{\nperson{\nhouseholdId\npersonId:personKey\nname\nfirstName\nlastName\n}\nreadableQuestions{\nid\nquestionText\nanswer{\ndisplayValue\n}\n}\n}\n}\n}",
+      "variables":{"eventId":"eeead714910ff206bf0cb4fccd51adad6961ca37af9c87eff"}
+    });
+    request.setRequestHeader("x-joy-personid",  guestId);
+    request.send(body);
   });
-  const body = JSON.stringify({
-    "query":"query($eventId:ID!){\nevent(eventId:$eventId){\nguestQuestions{\nperson{\nhouseholdId\npersonId:personKey\nname\n}\nreadableQuestions{\nid\nquestionText\nanswer{\ndisplayValue\n}\n}\n}\n}\n}",
-    "variables":{"eventId":"eeead714910ff206bf0cb4fccd51adad6961ca37af9c87eff"}
-  });
-  request.setRequestHeader("x-joy-personid", guestId);
-  request.send(body);
 }
 /**
  * @param {String} HTML representing a single element.
@@ -74,8 +74,8 @@ function createRsvpTableRow(guestName, guestId, answers) {
 }
 function displayGuestDetails() {
   const urlParams = new URLSearchParams(window.location.search);
-  const guestId = urlParams.get("id");
-  queryGuestDetails(guestId, (guestQuestions) => {
+  const guestIdPromise = urlParams.get("id");
+  queryGuestDetails(guestIdPromise, (guestQuestions) => {
     /*
 [
   {
@@ -136,29 +136,82 @@ function displayGuestDetails() {
     guestQuestions.forEach((guestQuestion) => {
       const guestId = guestQuestion.person.personId;
       const guestName = guestQuestion.person.name;
+      const guestFirstName = guestQuestion.person.firstName;
+      const guestLastName = guestQuestion.person.lastName;
       const answers = Object.assign({}, ...guestQuestion.readableQuestions.map((question) => ({[question.id]: question.answer.displayValue})));
       createRsvpTableRow(guestName, guestId, answers);
     });
   });
 }
-function findMatchingGuest() {
+async function updateRsvpFormWithGuestInfo(guestId) {
+
+  const form = document.getElementById("form");
+  const parentElement = form.parentElement;
+  const newForm = fromHTML('<form id="form"></form>');
+
+  // Get guest details from guestId
+  const guestQuestions = await queryGuestDetails(guestId);
+  guestQuestions.forEach((guestQuestion, index, array) => {
+    const guestName = guestQuestion.person.name;
+    const guestFirstName = guestQuestion.person.firstName;
+    const guestLastName = guestQuestion.person.lastName;
+    const answers = Object.assign({}, ...guestQuestion.readableQuestions.map((question) => ({[question.id]: question.answer.displayValue})));
+
+    console.log(guestName + guestId + JSON.stringify(answers)); // TODO remove log
+
+    const guestDetails = fromHTML('<div class="rsvp-answers"><div><span class="gravity-font">' + guestFirstName +
+      '</span><br><span class="maxi-font">' + guestLastName +
+      '</span></div><div class="radio-buttons rsvp-answers"><label class="sq-radio" for="yes-' + guestId +
+      '">Yes<input type="radio" id="yes-' + guestId + '" name="rsvp-' + guestId +
+      '" value="Yes" checked="checked"><span class="checkmark"></span></label><label class="sq-radio" for="no-' + guestId +
+      '">No<input type="radio" id="no-' + guestId + '" name="rsvp-' + guestId +
+      '" value="No"><span class="checkmark"></span></label></div></div>');
+    newForm.appendChild(guestDetails);
+    if (index !== array.length - 1){ 
+      const andDelimiter = fromHTML('<div class="rsvp-and">AND</div>');
+      newForm.appendChild(andDelimiter);
+    }
+  });
+
+  form.remove();
+  parentElement.appendChild(newForm);
+
+  // resize bottom sheet to new height
+  const bottomSheet = document.getElementById('bottomSheet');
+  const bottomSheetContent = document.getElementById('bottomSheetContent');
+  bottomSheet.style.height = bottomSheetContent.scrollHeight + "px";
+}
+async function findMatchingGuest() {
   updateText("Loading...");
   const fname = document.getElementById("fname").value;
   const lname = document.getElementById("lname").value;
-  const request = createGraphqlRequest((response) => {
-    const matchCount = response.data.event.getPersonMatchResult.matchCount;
-    if (matchCount > 0) {
-      const matchId = response.data.event.getPersonMatchResult.match.personKey;
-      window.location.href = "guest.html?id=" + matchId;
-    } else {
-      updateText("No guest found with name " + fname + " " + lname);
-    }
-  })
-  const body = JSON.stringify({
-    "query":"\nquery PersonMatchWithEmail($eventId: ID!, $firstName: String!, $lastName: String!, $email: String) {\n  event(eventId: $eventId) {\n    getPersonMatchResult(firstName: $firstName, lastName: $lastName, email: $email) {\nmatchCount\nmatch{\npersonKey\n}\n}\n}\n}\n",
-    "variables":{"eventId":"eeead714910ff206bf0cb4fccd51adad6961ca37af9c87eff","firstName":fname,"lastName":lname,"email":""}
+  const guestId = await new Promise((resolve, reject) => {
+    const request = createGraphqlRequest();
+    request.onload = () => {
+      if (request.readyState == 4 && request.status == 200) {
+        const response = JSON.parse(request.responseText);
+        const matchCount = response.data.event.getPersonMatchResult.matchCount;
+        if (matchCount > 0) {
+          const matchId = response.data.event.getPersonMatchResult.match.personKey;
+          resolve(matchId);
+        } else {
+          resolve();
+        }
+      } else {
+        reject(`Error: ${request.status}`);
+      }
+    };
+    const body = JSON.stringify({
+      "query":"\nquery PersonMatchWithEmail($eventId: ID!, $firstName: String!, $lastName: String!, $email: String) {\n  event(eventId: $eventId) {\n    getPersonMatchResult(firstName: $firstName, lastName: $lastName, email: $email) {\nmatchCount\nmatch{\npersonKey\n}\n}\n}\n}\n",
+      "variables":{"eventId":"eeead714910ff206bf0cb4fccd51adad6961ca37af9c87eff","firstName":fname,"lastName":lname,"email":""}
+    });
+    request.send(body);
   });
-  request.send(body);
+  if (typeof guestId !== "undefined") {
+    updateRsvpFormWithGuestInfo(guestId);
+  } else {
+    updateText("No guest found with name " + fname + " " + lname);
+  }
 }
 function sendRsvp() {
   const fname = document.getElementById("fname").value;
@@ -263,7 +316,7 @@ function hideRsvpModal() {
   modal.style.display = "none";
 }
 function showBottomSheet() {
-  const bottomSheet = document.getElementById('bottomSheet')
+  const bottomSheet = document.getElementById('bottomSheet');
   bottomSheet.style.height = bottomSheet.scrollHeight + "px";
   document.getElementById("modal-overlay").style.display = "unset";
 }
