799344b52a48267b98a56d5e489a7a536acca2fa
RJ Jaisimha <jaisimha@amazon.com>
2024-06-08 20:43:21 +0200

    Implement support for back-end at api.adtrj.com/rsvp
    Also copied getaway FAQs to reception for testing UX

--- README.md
+++ README.md
@@ -1,11 +1,13 @@
 # ADTRJ
 
-Fully front-end website which interfaces with withjoy.com for RSVP tracking.
+Front-end website for RSVP tracking
+
+Note: this website used to use withjoy.com as a back-end but it stopped working so we developed our own back-end at api.adtrj.com/rsvp
 
 ## Running locally
 
 `./tools/server.sh`
 
-## Test graphql queries to withjoy.com
+## ~~Test graphql queries to withjoy.com~~
 
-Update query.txt, variables.txt, and headers.txt as necessary. Then run `./query_graphql.sh`.
+~~Update query.txt, variables.txt, and headers.txt as necessary. Then run `./query_graphql.sh`.~~
--- src/rsvp.js
+++ src/rsvp.js
@@ -6,30 +6,6 @@ function updateText(text) {
   const para = document.getElementById("output");
   para.innerText = text;
 }
-function createGraphqlRequest() {
-  const xhr = new XMLHttpRequest();
-  xhr.open("POST", "https://ceremony-api.withjoy.com/graphql");
-  xhr.setRequestHeader("Content-Type", "application/json");
-  return xhr;
-}
-function queryGuestDetails(guestId) {
-  return new Promise((resolve, reject) => {
-    const request = createGraphqlRequest();
-    request.onload = () => {
-      if (request.readyState == 4 && request.status == 200) {
-        resolve(JSON.parse(request.responseText).data.event.guestQuestions);
-      } else {
-        reject(`Error: ${request.status}`);
-      }
-    };
-    const body = JSON.stringify({
-      "query":"query($eventId:ID!){\nevent(eventId:$eventId){\nguestQuestions{\nperson{\nhouseholdId\npersonId:personKey\nname\nfirstName\nlastName\n}\nreadableQuestions{\nid\nquestionText\nanswer{\ndisplayValue\n}\n}\n}\n}\n}",
-      "variables":{"eventId":"eeead714910ff206bf0cb4fccd51adad6961ca37af9c87eff"}
-    });
-    request.setRequestHeader("x-joy-personid",  guestId);
-    request.send(body);
-  });
-}
 /**
  * @param {String} HTML representing a single element.
  * @param {Boolean} flag representing whether or not to trim input whitespace, defaults to true.
@@ -50,104 +26,7 @@ function fromHTML(html, trim = true) {
   if (result.length === 1) return result[0];
   return result;
 }
-function createRsvpTableRow(guestName, guestId, answers) {
-  const row = fromHTML('<tr><td><div>' + guestName +
-    '<input type="radio" id="yes-'+guestId+'" name="rsvp-'+guestId+'" value="Yes">' +
-    '<label for="yes-'+guestId+'">Yes</label>' +
-    '<input type="radio" id="no-'+guestId+'" name="rsvp-'+guestId+'" value="No">' +
-    '<label for="no-'+guestId+'">No</label><br>' +
-    'Notes <input type="text" id="notes-'+guestId+'" name="notes"></div></td></tr>');
-  const table = document.getElementById("table");
-  table.append(row);
-  if (answers["rsvp"] === "Y") {
-    const button = document.getElementById("yes-" + guestId);
-    button.setAttribute("checked", "checked");
-  }
-  if (answers["rsvp"] === "N") {
-    const button = document.getElementById("no-" + guestId);
-    button.setAttribute("checked", "checked");
-  }
-  if (answers["funFact"]) {
-    const textInput = document.getElementById("notes-" + guestId);
-    textInput.setAttribute("placeholder", answers["funFact"]);
-  }
-}
-function displayGuestDetails() {
-  const urlParams = new URLSearchParams(window.location.search);
-  const guestIdPromise = urlParams.get("id");
-  queryGuestDetails(guestIdPromise, (guestQuestions) => {
-    /*
-[
-  {
-    "person": {
-      "householdId": "person-invitee-NrWtyR7vkrJ2H9Ox3rx",
-      "personId": "person-invitee-NrWtyR7vkrJ2H9Ox3rx",
-      "name": "Rahul Jaisimha"
-    },
-    "readableQuestions": [
-      {
-        "id": "rsvp",
-        "questionText": "Will you be able to join us at our wedding? Kindly reply by the date of ____.",
-        "answer": {
-          "displayValue": "Y"
-        }
-      },
-      {
-        "id": "address",
-        "questionText": "What is your mailing address?",
-        "answer": {
-          "displayValue": "address"
-        }
-      },
-      {
-        "id": "funFact",
-        "questionText": "Notes",
-        "answer": {
-          "displayValue": "love"
-        }
-      }
-    ]
-  },
-  {
-    "person": {
-      "householdId": "person-invitee-NrWtyR7vkrJ2H9Ox3rx",
-      "personId": "person-invitee-NrWtyV7hSV8I4xiP9dh",
-      "name": "Aditi Verma"
-    },
-    "readableQuestions": [
-      {
-        "id": "rsvp",
-        "questionText": "Will you be able to join us at our wedding? Kindly reply by the date of ____.",
-        "answer": {
-          "displayValue": "Y"
-        }
-      },
-      {
-        "id": "funFact",
-        "questionText": "Notes",
-        "answer": {
-          "displayValue": "boogaloo"
-        }
-      }
-    ]
-  }
-]
-    */
-    guestQuestions.forEach((guestQuestion) => {
-      const guestId = guestQuestion.person.personId;
-      const guestName = guestQuestion.person.name;
-      const guestFirstName = guestQuestion.person.firstName;
-      const guestLastName = guestQuestion.person.lastName;
-      const answers = Object.assign({}, ...guestQuestion.readableQuestions.map((question) => ({[question.id]: question.answer.displayValue})));
-      createRsvpTableRow(guestName, guestId, answers);
-    });
-  });
-}
-function getAnswers(guestQuestion) {
-  return Object.assign({}, ...guestQuestion.readableQuestions.map((question) => ({[question.id]: question.answer.displayValue})));
-}
-async function updateRsvpFormWithGuestInfo(guestId) {
-
+async function updateRsvpFormWithGuestInfo(guests, bangalore) {
   const form = document.getElementById("form");
   const parentElement = form.parentElement;
   const newForm = fromHTML('<form id="form"></form>');
@@ -155,25 +34,19 @@ async function updateRsvpFormWithGuestInfo(guestId) {
   function handleForm(event) { event.preventDefault(); } 
   newForm.addEventListener('submit', handleForm);
 
-  // Get guest details from guestId
-  const guestQuestions = await queryGuestDetails(guestId);
-
   form.remove();
   parentElement.appendChild(newForm);
 
-  const metadata = JSON.stringify(guestQuestions);
+  const metadata = JSON.stringify(guests);
   const metadataInput = fromHTML('<input type="hidden" id="metadata" name="metadata" value=\'' + metadata + '\'>');
   newForm.appendChild(metadataInput);
 
-  guestQuestions.forEach((guestQuestion, index, array) => {
-    const personId = guestQuestion.person.personId;
-    const guestName = guestQuestion.person.name;
-    const guestFirstName = guestQuestion.person.firstName;
-    const guestLastName = guestQuestion.person.lastName;
-    const answers = getAnswers(guestQuestion);
+  guests.forEach((guest, index, array) => {
+    const personId = guest.firstName + "-" + guest.lastName;
+    const rsvp = bangalore ? guest.bangaloreRsvp : guest.bentotaRsvp;
 
-    const guestDetails = fromHTML('<div class="rsvp-answers"><div><span class="gravity-font">' + guestFirstName +
-      '</span><br><span class="maxi-font">' + guestLastName +
+    const guestDetails = fromHTML('<div class="rsvp-answers"><div><span class="gravity-font">' + guest.firstName +
+      '</span><br><span class="maxi-font">' + guest.lastName +
       '</span></div><div class="radio-buttons rsvp-answers"><label class="sq-radio" for="yes-' + personId +
       '">Yes<input type="radio" id="yes-' + personId + '" name="rsvp-' + personId +
       '" value="Yes"><span class="checkmark"></span></label><label class="sq-radio" for="no-' + personId +
@@ -182,11 +55,11 @@ async function updateRsvpFormWithGuestInfo(guestId) {
 
     newForm.appendChild(guestDetails);
 
-    if (answers["rsvp"] === "Y") {
+    if (rsvp === true) {
       const button = document.getElementById("yes-" + personId);
       button.setAttribute("checked", "checked");
     }
-    if (answers["rsvp"] === "N") {
+    if (rsvp === false) {
       const button = document.getElementById("no-" + personId);
       button.setAttribute("checked", "checked");
     }
@@ -198,7 +71,7 @@ async function updateRsvpFormWithGuestInfo(guestId) {
   });
 
   const outputText = fromHTML('<p id="output"></p>');
-  const submitButton = fromHTML('<button class="rsvp noSelect color" onclick="saveGuestResponse()">ENTER</button>');
+  const submitButton = fromHTML('<button class="rsvp noSelect color" onclick="saveGuestResponse(' + bangalore + ')">ENTER</button>');
   newForm.appendChild(outputText);
   newForm.appendChild(submitButton);
 
@@ -209,151 +82,76 @@ async function updateRsvpFormWithGuestInfo(guestId) {
 }
 async function saveGuestResponse(bangalore) {
   updateText("Saving...");
-  const guestQuestions = JSON.parse(document.getElementById("metadata").value);
+  const guests = JSON.parse(document.getElementById("metadata").value);
   const promises = [];
-  guestQuestions.forEach((guestQuestion, index, array) => {
-    const personId = guestQuestion.person.personId;
-    const guestName = guestQuestion.person.name;
-    const guestFirstName = guestQuestion.person.firstName;
-    const guestLastName = guestQuestion.person.lastName;
-    const answers = getAnswers(guestQuestion);
-    const rsvpSelection = document.querySelector('input[name="rsvp-' + personId + '"]:checked').value === "Yes";
-    const bangaloreRsvp = bangalore ? "Y" : "N";
-    promises.push(sendRsvp(guestFirstName, guestLastName, personId, rsvpSelection, bangaloreRsvp));
+  guests.forEach((guest, index, array) => {
+    const personId = guest.firstName + "-" + guest.lastName;
+    const rsvpSelected = document.querySelector('input[name="rsvp-' + personId + '"]:checked');
+    if (rsvpSelected !== null) {
+      rsvp = rsvpSelected.value === "Yes";
+      if (bangalore) {
+        guest.bangaloreRsvp = rsvp;
+      } else {
+        guest.bentotaRsvp = rsvp;
+      }
+    }
+    promises.push(sendRsvp(guest));
   });
   await Promise.all(promises);
   updateText("Saved");
 }
-async function findMatchingGuest() {
+async function findMatchingGuest(bangalore) {
   updateText("Loading...");
   const fname = document.getElementById("fname").value;
   const lname = document.getElementById("lname").value;
-  const guestId = await new Promise((resolve, reject) => {
-    const request = createGraphqlRequest();
+  const guests = await new Promise((resolve, reject) => {
+    const request = new XMLHttpRequest();
+    request.open("GET", "https://api.adtrj.com/rsvp/?firstName=" + fname + "&lastName=" + lname);
     request.onload = () => {
       if (request.readyState == 4 && request.status == 200) {
-        const response = JSON.parse(request.responseText);
-        const matchCount = response.data.event.getPersonMatchResult.matchCount;
-        if (matchCount > 0) {
-          const matchId = response.data.event.getPersonMatchResult.match.personKey;
-          resolve(matchId);
-        } else {
-          resolve();
-        }
+        resolve(JSON.parse(request.responseText));
       } else {
         reject(`Error: ${request.status}`);
       }
     };
-    const body = JSON.stringify({
-      "query":"\nquery PersonMatchWithEmail($eventId: ID!, $firstName: String!, $lastName: String!, $email: String) {\n  event(eventId: $eventId) {\n    getPersonMatchResult(firstName: $firstName, lastName: $lastName, email: $email) {\nmatchCount\nmatch{\npersonKey\n}\n}\n}\n}\n",
-      "variables":{"eventId":"eeead714910ff206bf0cb4fccd51adad6961ca37af9c87eff","firstName":fname,"lastName":lname,"email":""}
-    });
-    request.send(body);
+    request.send();
   });
-  if (typeof guestId !== "undefined") {
-    updateRsvpFormWithGuestInfo(guestId);
+  if (guests.length > 0) {
+    updateRsvpFormWithGuestInfo(guests, bangalore);
   } else {
     updateText("No guest found with name " + fname + " " + lname);
   }
 }
-function sendRsvp(fname, lname, personId, rsvpSelection, bangaloreRsvp) {
-  const inviteeId = personId.replace('person-invitee', '');
+function sendRsvp(guest) {
   return new Promise((resolve, reject) => {
     const request = new XMLHttpRequest();
-    request.open("POST", 'https://ceremony-api.withjoy.com/events/eeead714910ff206bf0cb4fccd51adad6961ca37af9c87eff/invites/' + inviteeId + '/sendRSVPCompleteToUser');
+    request.open("POST", "https://api.adtrj.com/rsvp/");
     request.setRequestHeader("Content-Type", "application/json");
     request.onload = () => {
       if (request.readyState == 4 && request.status == 200) {
-        resolve(request.responseText);
+        resolve(JSON.parse(request.responseText));
       } else {
         reject(`Error: ${request.status}`);
       }
     };
-    const body = JSON.stringify({
-      "targetInviteeData": {
-        "name": fname + " " + lname,
-        "email": "",
-        "personKey": "person-invitee" + inviteeId,
-        "keys": {
-          "userIds": [],
-          "inviteeIds": [
-            inviteeId
-          ]
-        }
-      },
-      "rsvpResponses": [
-        {
-          "key": "contact",
-          "question": "What is your contact info?",
-          "title": "Contact Info",
-          "answer": {
-            "personKey": "person-invitee" + inviteeId,
-            "name": fname + " " + lname,
-            "email": "",
-            "keys": {
-              "users": [],
-              "invitees": [
-                inviteeId
-              ]
-            }
-          }
-        },
-        {
-          "key": "rsvp",
-          "question": "Will you be able to join us at our wedding? Kindly reply by the date of ____.",
-          "title": "RSVP",
-          "answer": "Joyfully Accept",
-          "boolean_answer": rsvpSelection
-        },
-        {
-          "key": "address",
-          "question": "When does your flight arrive?",
-          "title": "arrival",
-          "answer": ""
-        },
-        {
-          "key": "funFact",
-          "question": "When does your flight depart?",
-          "title": "departure",
-          "answer": ""
-        },
-        {
-          "key": "-NvTpKRM_M8iI6U5AVO2",
-          "question": "Will you be attending Bangalore?",
-          "title": "Bangalore",
-          "answer": bangaloreRsvp
-        }
-      ],
-      "getTheAppLinkData": {
-        "eventId": "eeead714910ff206bf0cb4fccd51adad6961ca37af9c87eff",
-        "firstName": fname,
-        "lastName": lname,
-        "email": "",
-        "personKey": "person-invitee" + inviteeId,
-        "personData": {
-          "userIds": [],
-          "inviteeIds": [
-            inviteeId
-          ]
-        }
-      }
-    });
+    const body = JSON.stringify(guest);
     request.send(body);
   });
 }
-function showRsvpModal() {
-  const modal = document.getElementById("form-modal");
-  const overlay = document.getElementById("modal-overlay");
-  overlay.style.display = "unset";
-  modal.style.display = "unset";
-}
-function hideRsvpModal() {
-  const modal = document.getElementById("form-modal");
-  const overlay = document.getElementById("modal-overlay");
-  overlay.style.display = "none";
-  modal.style.display = "none";
-}
-function showBottomSheet() {
+function showBottomSheet(bangalore) {
+  // Update the find guest button with whether or not they are rsvp-ing for the Bangalore event
+  const button = document.getElementById('formButton');
+  if (button !== null) {
+    button.onclick = function() { findMatchingGuest(bangalore); };
+  }
+
+  // Update the form with the right content if available
+  const metadata = document.getElementById("metadata");
+  if (metadata !== null) {
+    const guests = JSON.parse(metadata.value);
+    updateRsvpFormWithGuestInfo(guests, bangalore);
+  }
+
   const bottomSheet = document.getElementById('bottomSheet');
   bottomSheet.style.height = bottomSheet.scrollHeight + "px";
   document.getElementById("modal-overlay").style.display = "unset";
